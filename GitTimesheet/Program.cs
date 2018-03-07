d96b89b3c5cccc54c04dd88f5fdea20305f7d98a
Richard Astbury <richard.astbury@gmail.com>
2018-03-07 14:33:54 +0000

    redesign

--- GitTimesheet/Program.cs
+++ GitTimesheet/Program.cs
@@ -1,172 +1,97 @@
 ﻿using LibGit2Sharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 class Program
 {
 
-    static void Main(string[] vargs)
+    class CommitDetails
     {
-        var args = vargs.Select(x => x.ToLower()).ToArray();
-
-        if (args.Contains("-help") || args.Contains("--help") || args.Contains("-h") || args.Contains("-?") || args.Contains("/?"))
+        public CommitDetails(DateTimeOffset date, string repo, string branch)
         {
-            Console.WriteLine(@"
-GitTimesheet
-
-Optional arguments:
-    
-    -month 12              (shows activity in December)
-    -year 2017             (shows activity in 2017)
-    -user dave@hotmail.com (shows activity of a single user)
-
-By default, all activity for the current month is shown
-");
-            return;
+            this.Date = date.Date.Date;
+            this.Repository = repo;
+            this.Branch = branch;
         }
-        
-
-        var parsedArgs = ParseArgs(args).ToArray();
-        var year = int.Parse(parsedArgs.GetOrDefault("-year", DateTime.UtcNow.Year.ToString()));
-        var month = int.Parse(parsedArgs.GetOrDefault("-month", DateTime.UtcNow.Month.ToString()));
-        var days = int.Parse(parsedArgs.GetOrDefault("-days", "0"));
-        var user = parsedArgs.GetOrDefault("-user", "").ToLower();
-        var repos = parsedArgs.GetOrDefault("-dir", ".").Split(',').Select(x => x.Trim()).ToArray();
-        var format = parsedArgs.GetOrDefault("-format", "report");
+        public DateTime Date { get; set; }
+        public string Repository { get; set; }
+        public string Branch { get; set; }
+    }
 
 
+    static void Main(string[] args)
+    {
+        var repos = args;
+        if (!args.Any())
+        {
+            repos = new string[] { "." };
+        }
+
+        var days = 7;
+
+        var startDate = new DateTimeOffset(DateTime.Now.AddDays(-days).Date);
+        var commits = Query(startDate, repos).ToArray();
+        foreach (var summary in ToSummary(commits, startDate, days))
+        {
+            Console.WriteLine(summary);
+        }
+    }
+
+
+
+    static IEnumerable<CommitDetails> Query(DateTimeOffset startDate, params string[] repos)
+    {
         foreach (var repoDir in repos)
         {
-            if (repos.Length > 1) Console.WriteLine(repoDir);
-
+            var repoName = Directory.GetParent(Path.Combine(repoDir, ".git")).Name;
             using (var repo = new Repository(repoDir))
             {
-                if (user == "me")
+                var user = "";
+                var userConfigValue = repo.Config.FirstOrDefault(x => x.Key == "user.email");
+                if (null != userConfigValue) user = userConfigValue.Value;
+
+                foreach (var branch in repo.Branches.Where(x => x.IsTracking))
                 {
-                    var userConfigValue = repo.Config.FirstOrDefault(x => x.Key == "user.email");
-                    if (null != userConfigValue) user = userConfigValue.Value;
-                }
-
-                var query = days > 0 ? GetLastNDays(repo, days, user) : GetAllPeriods(repo, year, month, user);
-
-                if (format == "report")
-                {
-
-                    foreach (var line in FormatTimesheets(query.ToArray()))
+                    // out of date branch
+                    if (branch.Tip.Committer.When < startDate) continue;
+                    foreach (var commit in branch.Commits.Where(x => x.Committer.When > startDate && x.Committer.Email == user))
                     {
-                        Console.WriteLine(line);
+                        yield return new CommitDetails(commit.Committer.When, repoName, branch.FriendlyName);
                     }
                 }
-                else
-                {
-                    foreach (var line in FormatSummary(query.ToArray()))
-                    {
-                        Console.WriteLine(line);
-                    }
-                }
+
             }
         }
     }
 
-    static IEnumerable<KeyValuePair<string, string>> ParseArgs(string[] args)
+    static IEnumerable<string> ToSummary(IEnumerable<CommitDetails> commits, DateTimeOffset startDate, int days)
     {
-        for (var i = 0; i < args.Length-1; i+=2)
+        for (var i = 0; i <= days; i++)
         {
-            yield return new KeyValuePair<string, string>(args[i], args[i + 1]);
-        }
-    }
+            var day = startDate.AddDays(i).Date;
+            yield return $"{day:yyyy-MM-dd ddd}";
 
-
-    static IEnumerable<Commit> GetAllPeriods(Repository repo, int year = -1, int month = -1, string userEmail = null)
-    {
-        IEnumerable<Commit> query = repo.Commits;
-
-        if (!string.IsNullOrWhiteSpace(userEmail)) query = query.Where(x => x.Committer.Email == userEmail);
-        if (year != -1) query = query.Where(x => x.Committer.When.ToLocalTime().Year == year);
-        if (month != -1) query = query.Where(x => x.Committer.When.ToLocalTime().Month == month);
-
-        return query;
-    }
-
-
-    static IEnumerable<Commit> GetLastNDays(Repository repo, int days, string userEmail = null)
-    {
-        IEnumerable<Commit> query = repo.Commits;
-        var startDate = DateTimeOffset.Now.AddDays(-days); ;
-        if (!string.IsNullOrWhiteSpace(userEmail)) query = query.Where(x => x.Committer.Email == userEmail);
-        query = query.Where(x => x.Committer.When.ToLocalTime() > startDate);
-
-        return query;
-    }
-
-
-    static IEnumerable<string> FormatSummary(Commit[] commits)
-    {
-        var commiters = commits.Select(x => x.Committer.Email.ToLower()).Distinct().ToArray();
-        foreach (var commiter in commiters)
-        {
-            yield return $"Commits by {commiter}";
-            var subsetByCommitter = commits.Where(x => x.Committer.Email.ToLower() == commiter).ToArray();
-            foreach (var commitsByDay in subsetByCommitter.GroupBy(x => x.Committer.When.ToString("yyyy-MM-dd ddd")))
+            var any = false;
+            foreach (var commitsOnThisDay in commits.Where(x => x.Date == day).GroupBy(x => $@"{x.Repository}\{x.Branch}"))
             {
-                yield return $"Commits on {commitsByDay.Key}";
-                foreach (var commit in commitsByDay)
-                {
-                    yield return $"  {commit.MessageShort}";
-                }
-
+                any = true;
+                yield return $"  {commitsOnThisDay.Key} : {commitsOnThisDay.Count()}";
             }
-        }
-    }
 
-    static IEnumerable<string> FormatTimesheets(Commit[] commits)
-    {
-        var grandTotal = 0;
-        var commiters = commits.Select(x => x.Committer.Email.ToLower()).Distinct().ToArray();
-        foreach (var commiter in commiters)
-        {
-            yield return $"Commits by {commiter}";
-            var subsetByCommitter = commits.Where(x => x.Committer.Email.ToLower() == commiter).ToArray();
-            foreach (var year in subsetByCommitter.Select(x => x.Committer.When.Year).Distinct().OrderBy(x => x))
+            if (!any)
             {
-                var subsetByYear = subsetByCommitter.Where(x => x.Committer.When.Year == year).ToArray();
-                foreach (var month in subsetByYear.Where(x => x.Committer.When.Year == year).Select(x => x.Committer.When.Month).Distinct().OrderBy(x => x))
-                {
-                    var subsetByMonth = subsetByYear.Where(x => x.Committer.When.Month == month).ToArray();
-                    yield return $"  {commiter}'s commits for {new DateTime(year, month, 1).ToString("MMMM")} {year}";
-
-                    var count = 0;
-                    // we're at a month for a given commiter
-                    foreach (var day in Enumerable.Range(1, DateTime.DaysInMonth(year, month)))
-                    {
-                        var date = new DateTime(year, month, day);
-                        var dailyCommitCount = subsetByMonth.Count(x => x.Committer.When.DayOfYear == date.DayOfYear);
-                        var message = dailyCommitCount > 0 ? $"{dailyCommitCount} commits" : "";
-                        if (dailyCommitCount > 0)
-                        {
-                            count++;
-                            grandTotal++;
-                        }
-                        yield return $"    {date.ToString("yyyy-MM-dd ddd")} {message}";
-                    }
-                    yield return $"  MONTHLY TOTAL = {count} days";
-                    yield return "";
-
-                }
+                yield return "  --none--";
             }
+            yield return "";
+
         }
-        yield return $"GRAND TOTAL = {grandTotal} days";
+
+
     }
+
 
 
 }
 
-public static class Extensions
-{
-    public static string GetOrDefault(this KeyValuePair<string, string>[] values, string key, string defaultValue)
-    {
-        if (!values.Any(x => x.Key == key)) return defaultValue;
-        return values.First(x => x.Key == key).Value;
-    }
-}
--- readme.md
+++ readme.md
@@ -5,63 +5,46 @@ Creates a timesheet report using your git history
 ## Example Output
 
 ```
-Commits by dave@hotmail.com
-  dave@hotmail.com's commits for October
-    2015-10-01 Thu 1 commits
-    2015-10-02 Fri 1 commits
-    2015-10-03 Sat
-    2015-10-04 Sun
-    2015-10-05 Mon 1 commits
-    2015-10-06 Tue
-    2015-10-07 Wed
-    2015-10-08 Thu
-    2015-10-09 Fri 1 commits
-    2015-10-10 Sat
-    2015-10-11 Sun
-    2015-10-12 Mon 1 commits
-    2015-10-13 Tue 12 commits
-    2015-10-14 Wed
-    2015-10-15 Thu 7 commits
-    2015-10-16 Fri 2 commits
-    2015-10-17 Sat 1 commits
-    2015-10-18 Sun
-    2015-10-19 Mon 3 commits
-    2015-10-20 Tue 2 commits
-    2015-10-21 Wed 1 commits
-    2015-10-22 Thu 7 commits
-    2015-10-23 Fri 2 commits
-    2015-10-24 Sat 3 commits
-    2015-10-25 Sun
-    2015-10-26 Mon 5 commits
-    2015-10-27 Tue 3 commits
-    2015-10-28 Wed 4 commits
-    2015-10-29 Thu 11 commits
-    2015-10-30 Fri 3 commits
-    2015-10-31 Sat
-  MONTHLY TOTAL = 20 days
+2018-02-28 Wed
+  project1\dev : 2
 
-GRAND TOTAL = 20 days
+2018-03-01 Thu
+  project1\master : 5
+
+2018-03-02 Fri
+  project1\test : 2
+
+2018-03-03 Sat
+  --none--
+
+2018-03-04 Sun
+  --none--
+
+2018-03-05 Mon
+  project1\master : 4
+
+2018-03-06 Tue
+  project1\master : 3
+  project1\test : 1
+
+2018-03-07 Wed
+  --none--
 ```
 
 ## Usage
 
-To run a report for all users for the current month:
+To run a report for your user for the last 7 days in the current repo:
 
 ```
 $ GitTimesheet
 ```
 
-To run a report for a different month:
+To run a report for multiple repos:
 
 ```
-$ GitTimesheet -year 2015 -month 10
+$ GitTimesheet path\to\repo1 path\to\repo2
 ```
 
-Just report on a single user:
-
-```
-$ GitTimesheet -user dave@hotmail.com
-```
 
 ## License
 
